381fe2867fc5aabef6e8feb591ef44d04d53d7b5
lirui <634178463@qq.com>
2018-02-13 21:48:56 +0800

    pymysql 使用。匹配 3.x

--- mysql/mysql.py
+++ mysql/mysql.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 
 
-# 确保 python 版本一致：2.7.10
-import MySQLdb
+# 确保 python 版本一致 3.x
+import pymysql
 
 # 连接数据库
-db = MySQLdb.connect("localhost", "root", "199011081108", "python")
+db = pymysql.connect("localhost", "root", "199011081108", "python")
 
 # 获取操作游标
 curse = db.cursor()
