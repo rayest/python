4b8583bc314caa17ff970c74a6ec5a9044a7ef92
lirui <634178463@qq.com>
2018-02-17 23:39:39 +0800

    自定义错误页面

--- template/template.py
+++ template/template.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
@@ -12,6 +13,16 @@ def index():
 @app.route('/user/<user_name>')
 def user(user_name):
     return render_template('user.html', userName=user_name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
