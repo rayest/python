b5c4533eb3e13412b0de12176da3c2b801ecf9aa
lirui <634178463@qq.com>
2018-02-19 01:04:32 +0800

    协程APP路由

---
+++ webapp/www/app.py
@@ -0,0 +1,24 @@
+import logging
+from aiohttp import web
+from datetime import datetime
+import asyncio, os, json, time
+
+logging.basicConfig(level=logging.INFO)
+
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', headers={'content-type': 'text/html'})
+
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
