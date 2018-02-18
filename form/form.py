4dfb096de44591ec90f0c95399a5c201461e7b47
lirui <634178463@qq.com>
2018-02-18 19:25:55 +0800

    表单继承

--- form/form.py
+++ form/form.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
 
 
 class NameForm(Form):
