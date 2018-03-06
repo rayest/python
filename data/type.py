5fe5d333b25feacd54359e7fe8c25bd7067962d5
lirui <634178463@qq.com>
2018-03-06 14:00:47 +0800

    规范化命名

--- data/type.py
+++ data/type.py
@@ -4,26 +4,23 @@
 # python 对缩进要求很严格，请注意
 # 1. 整数
 a = 5
-print a
-
+print(a)
 
 # 2. 字符串
 b = "hello"
 c = 'hello C'
 d = 'hello C'
-print b
-print c
+print(b)
+print(c)
 
 # 3. 布尔值 True 及 False
 e = 'hello e'
 f = 'hello f'
-print e == f
-print e != f
+print(e == f)
+print(e != f)
 
 score = 80
 if score >= 60:
-    print "passed"
+    print("passed")
 else:
-    print "not passed"
-
-
+    print("not passed")
