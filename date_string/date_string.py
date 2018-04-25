965c24438188e938072438b61c9489f19f470153
lirui <634178463@qq.com>
2018-04-25 13:58:15 +0800

    Learn about date.

--- date_string/date_string.py
+++ date_string/date_string.py
@@ -3,10 +3,17 @@ from datetime import datetime
 
 
 def to_utc(x):
-    d = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(x*0.001))
-
+    d = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(x * 0.001))
     print(d)
 
 
+Y_M_D_H_M_S = '%Y-%m-%d %H:%M:00'
+UTC = '%Y-%m-%dT%H:%M:%SZ'
+
+
+def current():
+    return datetime.now().strftime(UTC)
+
+
 if __name__ == '__main__':
-    to_utc(1523944499474)
+    print(current())
