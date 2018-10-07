50a9e8bb4af4d013979584a84e11bb7631c5282e
lirui <634178463@qq.com>
2018-10-07 19:08:49 +0800

    optimize rabbitmq

--- rabbitmq/hello_world_consumer.py
+++ rabbitmq/hello_world_consumer.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python
 import pika
+credentials = pika.PlainCredentials('guest', 'guest')
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection_params = pika.ConnectionParameters(host='localhost', credentials=credentials)
+connection = pika.BlockingConnection(connection_params)
 channel = connection.channel()
+channel.exchange_declare(exchange='hello-exchange',
+                         exchange_type='direct',
+                         passive=False,
+                         durable=True,
+                         auto_delete=False)
+channel.queue_declare(queue='hello-queue')
+channel.queue_bind(queue='hello-queue', exchange='hello-exchange', routing_key='hola')
 
-channel.queue_declare(queue='hello')
 
+def msg_consumer(channel, method, header, body):
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+    if body == 'quit':
+        channel.basic_channel(consumer_tag='hello-consumer')
+        channel.stop_consuming()
 
-def callback(ch, method, properties, body):
+    else:
+        print(body)
     print(" [x] Received %r" % body)
 
 
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+channel.basic_consume(msg_consumer,
+                      queue='hello-queue',
+                      consumer_tag='hello-consumer')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- rabbitmq/hello_world_producer.py
+++ rabbitmq/hello_world_producer.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 import pika
+import sys
 
 credentials = pika.PlainCredentials('guest', 'guest')
-parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials)
-connection = pika.BlockingConnection(parameters)
+parameters = pika.ConnectionParameters('localhost', credentials=credentials)
+connection = pika.BlockingConnection(parameters) # 建立到代理服务器的连接 rabbitMQ
 
 # connection = pika.BlockingConnection(pika.connectionParameters(host='localhost'))
-channel = connection.channel()
+channel = connection.channel() # 获得信道
 
-channel.queue_declare(queue='hello')
+# 声明交换器
+channel.exchange_declare(exchange='hello-exchange',
+                         exchange_type='direct',
+                         passive=False,
+                         durable=True,
+                         auto_delete=False)
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
+# 创建纯文本消息
+msg = sys.argv[1]
+msg_props = pika.BasicProperties()
+msg_props.content_type = 'text/plain'
+
+# 发布消息
+channel.basic_publish(exchange='hello-exchange',
+                      routing_key='hola',
+                      body=msg,
+                      properties=msg_props)
 print(" [x] Sent 'Hello World!'")
 connection.close()
